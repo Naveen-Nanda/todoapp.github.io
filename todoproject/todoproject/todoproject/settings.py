caf8985e1fa42508dfa4ad302343dacdcdb23489
Naveen-Nanda <navikamob@gmail.com>
2019-05-21 15:06:43 -0700

    Update settings.py

--- todoproject/todoproject/todoproject/settings.py
+++ todoproject/todoproject/todoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't+=ptzdp(xia&()#ooyc=i7!n*qaxsm2%7wz5+%l*8$q0jial#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nandaiitapp.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,3 +135,22 @@ EMAIL_HOST_PASSWORD = 'Footloose9677'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'codesq/static/')
 STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
